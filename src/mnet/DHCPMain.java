97d6a187f0e0a1476dbb1d0d9be912c05e4adf19
milad hero <miladbashiri@yahoo.com>
2011-03-17 13:55:00 +0330

    Server can capture incoming packets. packet contents can be shown with getBinary, getHex, or getData.

--- src/mnet/DHCPMain.java
+++ src/mnet/DHCPMain.java
@@ -1,10 +1,18 @@
 package mnet;
 
+import java.net.SocketException;
+
 public class DHCPMain {
 	public static void main(String[] args){
-		System.out.println("DHCP-Server Started...");
-		DHCPServer server = new DHCPServer();
-		server.listen();
+		try{
+			DHCPServer server = new DHCPServer();
+			server.capturePacket(300);
+			System.out.println("Packet content in binary: ");
+			System.out.println(server.getBinary());
+			System.out.println("===========================\n" +
+					"Packet Content in Hex: ");
+			System.out.println(server.getHex());
+		}catch(SocketException e){e.printStackTrace();};
 	}
 
 }
--- src/mnet/DHCPServer.java
+++ src/mnet/DHCPServer.java
@@ -1,37 +1,60 @@
 package mnet;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.*;
+import java.util.BitSet;
 public class DHCPServer {
-	private static final int PACKET_LENGTH = 1, MINI_PACKET = 1;
+	
 	private DatagramSocket socket;
-	public DHCPServer(){
+	private DatagramPacket packet;
+	private byte[] data;
+	private String binaryStr, hexStr;
+	public DHCPServer() throws SocketException{
 		try{
 			socket = new DatagramSocket(67);
-		}catch(SocketException e){e.printStackTrace();}
+			System.out.println("DHCP Server Created.\n" +
+					   "Use capturePacket to capture an incoming request.");
+		}catch(SocketException e){
+			System.out.println("\nAn error occurred creating socket on port 67.\n" + 
+					"If running under linux, try using sudo.\n");
+			throw e;
+		}
 	}
-	public void listen(){
-		byte[] data = new byte[PACKET_LENGTH];
-		DatagramPacket packet = new DatagramPacket(data, PACKET_LENGTH);
-		try {
-			System.out.println("Listening Started.");
-			System.out.println("packet size to expect: " + PACKET_LENGTH);
+	public void capturePacket(int packetLength){
+		binaryStr = null;
+		hexStr = null;
+		data = new byte[packetLength];
+		DatagramPacket packet = new DatagramPacket(data, packetLength);
+		System.out.println("Listening started on port 67...");
+		try{
 			socket.receive(packet);
-			System.out.println(new String(data));
-			System.out.println("Packet Received.");
-		} catch (IOException e) { e.printStackTrace(); }
+			System.out.println("Packet captured.");
+		}catch(IOException e){e.printStackTrace();}
 	}
-	public void countListen(){
-		byte[] data = new byte[MINI_PACKET];
-		DatagramPacket packet = new DatagramPacket(data, MINI_PACKET);
-		try {
-			System.out.println("Listening Started.");
-			int count = 0;
-			while(true){
-				count++;
-				socket.receive(packet);
-				System.out.println(count);
-			}
-		} catch (IOException e) { e.printStackTrace(); }
+	
+	public String getBinary(){
+		if(data == null) return null;
+		if(binaryStr != null)
+			return binaryStr;
+		binaryStr = "";
+		for(int i = 0; i < data.length; i++)
+			binaryStr = binaryStr + Helper.getBinary(data[i]);
+		return binaryStr;
 	}
-
+	public String getHex(){
+		if(data == null) return null;
+		if(hexStr != null)
+			return hexStr;
+		hexStr = "";
+		for(int i = 0; i < data.length; i++)
+			hexStr = hexStr + Helper.getHex(data[i]);
+		return hexStr;
+	}
+	
+	public byte[] getData(){
+		return data;
+	}
+	public DatagramPacket getPacket(){
+		return packet;
+	}
 }
